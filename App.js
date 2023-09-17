fbbdd2c70ed169bac3889fb0702ee8888a55e99d
Hiago Gomes Pereira <hiagogomespereira@MacBook-Pro-de-Hiago.local>
2023-09-17 11:33:16 -0300

    mudando a cor da calculadora

--- App.js
+++ App.js
@@ -80,12 +80,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#483d8b'}]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'white': 'silver'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -101,10 +101,10 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#483d8b"
   },
   resultText: {
-    color: "#282F38",
+    color: "white",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
@@ -121,15 +121,16 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: 'rgb(71, 12, 122)',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90, 
     minHeight: 90,
     flex: 2,
+    
   },
   textButton: {
-    color: "#7c7c7c",
+    color: "rgb(71, 12, 122)",
     fontSize: 20,
   } 
 });
